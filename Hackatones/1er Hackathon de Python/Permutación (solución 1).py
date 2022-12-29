7588c5e836d5b78d71659425ee5aee9c9d48068d
Ruddy Guerrero Alvarez <rguerrero9501@gmail.com>
2022-12-29 12:58:13 +0100

    Changes in the folder structures

--- Hackatones/1er Hackathon de Python/Permutación (solución 1).py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-lista = list(map(int, input().split(' ')))
-
-solucion = 0
-for i in range(1, n+1):
-    if i not in lista:
-        solucion += 1
-
-print(solucion)
--- Hackatones/1er Hackathon de Python/Permutación (solución 2).py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-lista = list(map(int, input().split(' ')))
-
-lista = set([x for x in lista if x <= n])
-
-print (n - len(lista))
